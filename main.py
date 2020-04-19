67b1a3e0896d1ff0112fe5b9ac85a2b6ce07d948
Alessandro Ferraioli <ferrass95@live.it>
2020-04-19 10:05:22 +0200

    fixed bug about dimensions

--- main.py
+++ main.py
@@ -1,49 +1,45 @@
 from Astar import *
 
 row = 20
-col = 20
+col = 30
 width = 800
 height = 800
+delay = 100
 
-sizeSquare = width/row
-
+sizeSquare = min(width/col,height/row)
 grid  = np.ones([row,col]) 
-
 
 root = Tk()
 #===================================================================================
 def callback(event):
     clickX = event.x
     clickY = event.y
-    grid[int(event.x/sizeSquare)][ int(event.y/sizeSquare)] = grid[int(event.x/sizeSquare)][ int(event.y/sizeSquare)] * -1
+    grid[ int(event.y/sizeSquare)][int(event.x/sizeSquare)] = grid[ int(event.y/sizeSquare)][int(event.x/sizeSquare)] * -1
 #===================================================================================
 def exit(e):
     root.destroy()
 #===================================================================================
-def draw(window,root):
+def createWalls(window,root):
     for x in range(row):
         for  y in range(col):
             top_left = [x*sizeSquare+1,y*sizeSquare+1]
             bottom_right  = [x*sizeSquare+ sizeSquare ,y*sizeSquare+sizeSquare]
             if(grid[x][y] == 1):
-                window.create_rectangle(top_left[0],top_left[1],bottom_right[0],bottom_right[1], fill='white')
+                window.create_rectangle(top_left[1],top_left[0],bottom_right[1],bottom_right[0], fill='white')
             else:
-                window.create_rectangle(top_left[0],top_left[1],bottom_right[0],bottom_right[1], fill='blue')
-    root.after(100,draw,window,root)
+                window.create_rectangle(top_left[1],top_left[0],bottom_right[1],bottom_right[0], fill='blue')
+    root.after(100,createWalls,window,root)
 #===================================================================================
 def main():
 
     root.bind("<Button-1>", callback)
     root.bind("<Escape>", exit)
-
     window = Canvas(root, width=width, height=height)
     window.pack()
-
-    root.after(100,draw,window,root)
-
+    root.after(100,createWalls,window,root)
     root.mainloop()
-
-    alg = Astar(width,height,row,col,[0,0],[row-1,col-1],100,grid)
+    print (grid.shape)
+    alg = Astar(width,height,row,col,[0,0],[row-1,col-1],delay,grid)
     
 #===================================================================================
 if __name__ == '__main__':
